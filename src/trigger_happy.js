140b331b299726479b299000a869dfeb591c3f74
Larry Myers <larry@larrymyers.com>
2010-11-15 22:58:44 -0500

    working firefox test

--- src/trigger_happy.js
+++ src/trigger_happy.js
@@ -2,7 +2,10 @@
     
     window.Trigger = {
         fire: function(elt, evtName, options) {
+            var evt = document.createEvent("MouseEvents");
             
+            evt.initMouseEvent(evtName, true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+            elt.dispatchEvent(evt);
         }
     };
 })();
